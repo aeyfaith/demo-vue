8f7dff41a97176848da1b2250c1625bdc4971ab6
aeyfaith <sameanmart@gmail.com>
2023-09-05 16:55:57 +0700

    test

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <router-link to="/demo">demo</router-link>
   </nav>
   <router-view/>
 </template>
